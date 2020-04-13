18b110f45c0856656c28462eaa8a3913e331d45b
yuhei.k <yuhei.k@gmail.com>
2020-04-13 19:52:49 +0900

    Test commit. Change main and external at once.
    Main. Update sprint value.
    External. Modified comment.

--- team_info.rb
+++ team_info.rb
@@ -1,7 +1,7 @@
 module Team
   SITE = "https://yuheik.atlassian.net"
   Project = "MYP"
-  Sprints = (1..1).to_a.map { |i| "#{Project} Sprint #{i}" }.flatten.reverse!
+  Sprints = (1..2).to_a.map { |i| "#{Project} Sprint #{i}" }.flatten.reverse!
   Members = [
   ]
 end
